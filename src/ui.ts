d6af5696c7f9c45185276fdccbbda9d278593a8e
Vitaly Turovsky <vital2580@icloud.com>
2022-03-12 15:28:27 +0300

    feat: add ability to use warn/error in `notificationConfirmAction`

--- src/ui.ts
+++ src/ui.ts
@@ -2,7 +2,13 @@ import * as vscode from 'vscode'
 
 // General UI actions (except quickpick)
 
-export const notificationConfirmAction = async (message: string, confirmButton: string) => {
-    const selectedAction = await vscode.window.showInformationMessage(message, confirmButton)
-    return selectedAction === confirmButton
+const typeConfirmAction = {
+    info: 'showInformationMessage',
+    warn: 'showWarningMessage',
+    error: 'showErrorMessage',
+}
+
+export const notificationConfirmAction = async (message: string, confirmButtonText: string, type: keyof typeof typeConfirmAction = 'info') => {
+    const selectedAction = await vscode.window[typeConfirmAction[type]](message, confirmButtonText)
+    return selectedAction === confirmButtonText
 }
