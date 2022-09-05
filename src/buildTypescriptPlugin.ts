29d5f4b09b6c0d2ba9a539d09806d64c21578142
Vitaly Turovsky <vital2580@icloud.com>
2022-09-05 21:25:55 +0300

    feat(buildTypescriptPlugin): expose esbuild options

--- src/buildTypescriptPlugin.ts
+++ src/buildTypescriptPlugin.ts
@@ -1,12 +1,12 @@
 import { join, resolve } from 'path'
 import { ensureDir } from 'fs-extra'
-import { build } from 'esbuild'
+import { build, BuildOptions } from 'esbuild'
 import { readPackageJsonFile, writePackageJsonFile } from 'typed-jsonfile'
 
 /**
  * @param entrypoint dir with src/index.ts and package.json if string
  */
-export default async (entrypoint: string | { file: string }, name?: string, outDir?: string) => {
+export default async (entrypoint: string | { file: string }, name?: string, outDir?: string, buildOptions: BuildOptions = {}) => {
     let packageJson: { name?: string; version?: string } | undefined
     if (typeof entrypoint === 'string') packageJson = await readPackageJsonFile({ dir: entrypoint })
     if (!name) {
@@ -33,5 +33,6 @@ export default async (entrypoint: string | { file: string }, name?: string, outD
         entryPoints: [resolve(...(typeof entrypoint === 'string' ? [entrypoint, 'src/index.ts'] : entrypoint.file))],
         outfile: join(outDir, 'index.js'),
         mainFields: ['module', 'main'],
+        ...buildOptions,
     })
 }
