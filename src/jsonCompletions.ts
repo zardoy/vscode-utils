cb16ba17ac4b989283e8ed8265aeebb4e09f04a4
Vitaly Turovsky <vital2580@icloud.com>
2022-09-07 20:34:02 +0300

    feat: add json completions utils

---
+++ src/jsonCompletions.ts
@@ -0,0 +1,51 @@
+import * as vscode from 'vscode'
+import { Location } from 'jsonc-parser'
+
+export function getJsonCompletingInfo(
+    location: Location,
+    document: vscode.TextDocument,
+    pos: vscode.Position,
+): false | { insideStringRange: vscode.Range | undefined } {
+    if (location.isAtPropertyKey) return false
+
+    const { previousNode } = location
+    if (previousNode) {
+        const offset = document.offsetAt(pos)
+        const insideNode = offset >= previousNode.offset && offset <= previousNode.offset + previousNode.length
+        if (!insideNode) return false
+        // perhaps string
+        const nodeStart = previousNode.offset + 1
+        const nodeEnd = nodeStart + previousNode.length - 2
+        return {
+            insideStringRange:
+                previousNode.type === 'string' && offset >= nodeStart && offset <= nodeEnd
+                    ? new vscode.Range(document.positionAt(nodeStart), document.positionAt(nodeEnd))
+                    : undefined,
+        }
+    }
+
+    return { insideStringRange: undefined }
+}
+
+export const jsonPathEquals = (path: Array<string | number>, toCompare: string[], endsWithNumber = false) => {
+    if (endsWithNumber) {
+        if (typeof path.slice(-1)[0] !== 'number') return false
+        path = path.slice(0, -1)
+    }
+
+    for (const [i, val] of toCompare.entries()) {
+        if (val === '*') path[i] = '*'
+        if (path[i] !== val) return false
+    }
+
+    return true
+}
+
+export const jsonValuesToCompletions = (items: string[], range: vscode.Range) =>
+    items.map(
+        (item): vscode.CompletionItem => ({
+            label: item,
+            kind: vscode.CompletionItemKind.Value,
+            range,
+        }),
+    )
