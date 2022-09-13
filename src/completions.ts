dcc234ec384024d7e21111e7603d41c8fd552c54
Vitaly Turovsky <vital2580@icloud.com>
2022-09-13 08:27:51 +0300

    feat: `niceLookingCompletion`

---
+++ src/completions.ts
@@ -0,0 +1,15 @@
+import * as vscode from 'vscode'
+
+// export let useCompletionNiceLook
+
+// export const prepareNiceLookingCompletinon = () => {}
+export const niceLookingCompletion = (ext: string, isFolderKind = false, fallbackKind = vscode.CompletionItemKind.Property) => {
+    return vscode.workspace.getConfiguration('workbench', null).get('iconTheme') === 'vscode-icons'
+        ? {
+              kind: isFolderKind ? vscode.CompletionItemKind.Folder : vscode.CompletionItemKind.File,
+              detail: ext,
+          }
+        : {
+              kind: fallbackKind,
+          }
+}
