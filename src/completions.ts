397ce2f145daca68c2e9e3386dd2fdd512eb41e9
Vitaly Turovsky <vital2580@icloud.com>
2022-09-13 08:30:21 +0300

    lint fix

--- src/completions.ts
+++ src/completions.ts
@@ -3,8 +3,8 @@ import * as vscode from 'vscode'
 // export let useCompletionNiceLook
 
 // export const prepareNiceLookingCompletinon = () => {}
-export const niceLookingCompletion = (ext: string, isFolderKind = false, fallbackKind = vscode.CompletionItemKind.Property) => {
-    return vscode.workspace.getConfiguration('workbench', null).get('iconTheme') === 'vscode-icons'
+export const niceLookingCompletion = (ext: string, isFolderKind = false, fallbackKind = vscode.CompletionItemKind.Property) =>
+    vscode.workspace.getConfiguration('workbench', null).get('iconTheme') === 'vscode-icons'
         ? {
               kind: isFolderKind ? vscode.CompletionItemKind.Folder : vscode.CompletionItemKind.File,
               detail: ext,
@@ -12,4 +12,3 @@ export const niceLookingCompletion = (ext: string, isFolderKind = false, fallbac
         : {
               kind: fallbackKind,
           }
-}
