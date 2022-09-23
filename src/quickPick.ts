ff8b7c285ad35066f6836e8261845c1cc9044189
Vitaly Turovsky <vital2580@icloud.com>
2022-09-23 18:26:13 +0300

    feat(quickPick): now passing this to events, add onDidShow & initialValue

--- src/quickPick.ts
+++ src/quickPick.ts
@@ -6,9 +6,10 @@ import { VSCodeQuickPickItem } from 'vscode-framework'
 type QuickPickCallbackOptions = Pick<vscode.QuickPick<any>, 'onDidChangeValue' | 'onDidTriggerButton' | 'onDidTriggerItemButton'>
 
 type QuickPickMethods = {
-    [K in keyof QuickPickCallbackOptions]?: QuickPickCallbackOptions[K] extends vscode.Event<infer U> ? (event: U) => void : never
+    [K in keyof QuickPickCallbackOptions]?: QuickPickCallbackOptions[K] extends vscode.Event<infer U> ? (this: vscode.QuickPick<any>, event: U) => void : never
 }
 
+// TODO allow to accept promise
 /** Shows a selection list. {@linkcode vscode.window.createQuickPick} wrapper. Enhenced version of {@linkcode vscode.window.showQuickPick} */
 export const showQuickPick = async <T, M extends boolean = false>(
     items: Array<VSCodeQuickPickItem<T>>,
@@ -16,7 +17,9 @@ export const showQuickPick = async <T, M extends boolean = false>(
         QuickPickMethods & {
             canPickMany?: M
             initialSelectedIndex?: number
-            onDidChangeActive?: (items: ReadonlyArray<VSCodeQuickPickItem<T>>, index: number) => any
+            onDidChangeActive?: (this: vscode.QuickPick<VSCodeQuickPickItem<T>>, items: ReadonlyArray<VSCodeQuickPickItem<T>>, index: number) => any
+            onDidShow?: (this: vscode.QuickPick<VSCodeQuickPickItem<T>>) => any
+            initialValue?: string
             // eslint-disable-next-line @typescript-eslint/ban-types
         } & (M extends true ? { onDidChangeSelection?: (items: ReadonlyArray<VSCodeQuickPickItem<T>>) => any } : {}) = {},
 ): Promise<(M extends true ? T[] : T) | undefined> => {
@@ -30,18 +33,20 @@ export const showQuickPick = async <T, M extends boolean = false>(
     Object.assign(quickPick, pickObject(options, ['ignoreFocusOut', 'matchOnDescription', 'matchOnDetail', 'placeHolder', 'title']))
     quickPick.placeholder = options.placeHolder
     quickPick.canSelectMany = options.canPickMany!
+    if (options.initialValue) quickPick.value = options.initialValue
     const { initialSelectedIndex, onDidChangeActive } = options
     const initialSelectedItem = initialSelectedIndex && items[initialSelectedIndex]
     if (initialSelectedItem) quickPick.activeItems = [initialSelectedItem]
     if (onDidChangeActive)
         quickPick.onDidChangeActive(newActiveItems => {
             const index = items.indexOf(newActiveItems[0]!)
-            onDidChangeActive(newActiveItems, index)
+            onDidChangeActive.call(quickPick, newActiveItems, index)
         })
+    const ignoreBindingCallbacks = new Set(['onDidChangeActive', 'onDidShow'])
     // bind callbacks
     // eslint-disable-next-line curly
     for (const [name, callback] of Object.entries(options)) {
-        if (name.startsWith('onDid') && name !== 'onDidChangeActive') quickPick[name](callback)
+        if (name.startsWith('onDid') && !ignoreBindingCallbacks.has(name)) quickPick[name]((callback as any).bind(quickPick))
     }
 
     const selectedValues = await new Promise<(M extends true ? T[] : T) | undefined>(resolve => {
@@ -57,6 +62,7 @@ export const showQuickPick = async <T, M extends boolean = false>(
             quickPick.hide()
         })
         quickPick.show()
+        options.onDidShow?.call(quickPick)
     })
 
     return selectedValues
