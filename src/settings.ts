861c172ffb96838e05f26fed7919af6a6fe55afb
Vitaly Turovsky <vital2580@icloud.com>
2022-10-24 00:44:11 +0300

    feat: add `watchExtensionSettings` util

--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,6 @@
+import * as vscode from 'vscode'
+import { Settings } from 'vscode-framework'
+
 // tests: https://github.com/zardoy/github-manager/tree/main/test/normalizeRegex.test.ts
 /** For *regex* type settings that are actually strings allows to specify flags e.g. test or /test/i */
 export const normalizeRegex = (input: string) => {
@@ -6,4 +9,14 @@ export const normalizeRegex = (input: string) => {
     const pattern = input.slice(1, -regexMatch[1]!.length - 1)
     const flags = regexMatch[1]
     return new RegExp(pattern, flags)
+}
+
+type SettingKey = keyof Settings
+
+export const watchExtensionSettings = (keys: SettingKey[], handler: (changedSettingKey: SettingKey) => any, scope?: vscode.ConfigurationScope) => {
+    vscode.workspace.onDidChangeConfiguration(({ affectsConfiguration }) => {
+        // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+        const changedConfigKey = keys.find(key => affectsConfiguration(key, scope))
+        if (changedConfigKey) handler(changedConfigKey)
+    })
 }
