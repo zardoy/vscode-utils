7457e57ad61a953368703ed75c4c3b1e2c855a0c
Vitaly Turovsky <vital2580@icloud.com>
2022-11-11 12:35:27 +0300

    add cjs watch script

--- postbuild.mjs
+++ postbuild.mjs
@@ -2,8 +2,14 @@ import fs from 'fs'
 
 const { include } = JSON.parse(fs.readFileSync('./tsconfig.cjs.json'))
 
+const fromWatch = process.argv.includes('--watch')
+
 for (const file of include) {
     const buildFile = `build/${file.slice('src/'.length, -3)}.js`
-    fs.renameSync(buildFile, `${buildFile.slice(0, -3)}.cjs`)
-    fs.renameSync(`${buildFile.slice(0, -3)}.d.ts`, `${buildFile.slice(0, -3)}.cjs.d.ts`)
+    try {
+        fs.renameSync(buildFile, `${buildFile.slice(0, -3)}.cjs`)
+        fs.renameSync(`${buildFile.slice(0, -3)}.d.ts`, `${buildFile.slice(0, -3)}.cjs.d.ts`)
+    } catch (err) {
+        if (!fromWatch) throw err
+    }
 }
